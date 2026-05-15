b8bde6423e826834ad79f0c89f5c8eff14b732df
rhnshrestha <shresthrohan447@gmail.com>
2026-05-15 22:17:29 +0545

    signup implementation

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "../backend/config/db.js";
 import authRoutes from "./routes/authRoutes.js"
+import cors from "cors"
 
 dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import { useState } from "react";
+import api from "../api/axios.js";
+import {useNavigate } from "react-router";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
 
-export default Signup
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [msg, setMsg] = useState("");
+
+  const handleChange = (e)=>{
+    setForm({
+      ...form,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    try {
+      const response = await api.post('/auth/signup', form);
+      setMsg(response.data.message);
+      navigate('/login');
+    } catch (error) {
+      setMsg(error.response?.data.message);
+    }
+  }
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
+      <div className="bg-white p-8 rounded-lg shadow-md  max-w-sm ">
+        <h2 className="mb-6 text-2xl font-bold text-center">Create Account</h2>
+
+        {msg && (
+          <div className="mb-4 text-center text-sm text-black-600 font-medium">
+            {msg}
+          </div>
+        )}
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Name"
+            value={form.name}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter Email"
+            value={form.email}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            value={form.password}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition-colors"
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
