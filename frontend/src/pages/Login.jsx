11334831ff1744f8bae2d51817e6c1875c3ad0fb
rhnshrestha <shresthrohan447@gmail.com>
2026-05-18 22:28:52 +0545

    login frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,70 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import api from "../api/axios";
 
 const Login = () => {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    email:"",
+    password: "",
+  });
+  const[msg, setMsg] = useState("");
 
-      <p className="">hello</p>
+  const handleChange = (e) => {
+    setForm({
+      ...form, 
+      [e.target.name] : e.target.value
+    })
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await api.post('/auth/login', form);
+      setMsg(response.data.message);
+      navigate('/');
+    } catch (error) {
+      setMsg(error.response?.data.message);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
+      <div className="bg-white p-8 rounded-lg shadow-md  max-w-sm ">
+        <h2 className="mb-6 text-2xl font-bold text-center">Login Here</h2>
+
+        {msg && (
+          <div className="mb-4 text-center text-sm text-black-600 font-medium">
+            {msg}
+          </div>
+        )}
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter Email"
+            value={form.email}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            value={form.password}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition-colors"
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
